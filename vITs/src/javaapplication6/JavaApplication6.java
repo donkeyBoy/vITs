c172c06ba17ec24b415eea7e4884a9a3f8d49af0
Splaytooth <Splaytooth@hotmail.com>
2015-04-07 12:49:58 +0200

    hej hej

--- vITs/src/javaapplication6/JavaApplication6.java
+++ vITs/src/javaapplication6/JavaApplication6.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication6;
 
 /**
@@ -11,7 +7,7 @@ package javaapplication6;
  */
 public class JavaApplication6 {
 
-    private String namn;
+    private String namn = "hej";
     /**
      * @param args the command line arguments
      */
