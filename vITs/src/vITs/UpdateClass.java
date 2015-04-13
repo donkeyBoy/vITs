ee836367d5fe634155358baebe6c5452483029eb
matus33 <enjoi_33@yahoo.com>
2015-04-13 11:45:47 +0200

    insert and login class

---
+++ vITs/src/vITs/UpdateClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vITs;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Matus
+ */
+public class UpdateClass {
+    
+    public static void main(String[] argv) {
+        try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
+                
+                Statement myStmt = connection.createStatement();
+                
+                String sql = "Insert into Lander values ('Zimbabwe')";
+                
+                myStmt.executeUpdate(sql);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
