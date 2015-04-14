7ef07e4496b6fc12f36a3c8bcb1aeae5605e8d1e
Stafffe <Steffan2f@yahoo.se>
2015-04-14 09:17:11 +0200

    Liten kombo-box

--- vITs/src/vITs/Inlogg.java
+++ vITs/src/vITs/Inlogg.java
@@ -5,10 +5,56 @@
  */
 package vITs;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Matus
  */
 public class Inlogg {
+
+    Connection conn = DatabasTest.newConnection();
     
+    
+    public Inlogg(){
+    
+    }
+    
+    public String login(String username, String losen) {
+        try {
+            Statement myStmt = conn.createStatement();
+            String sql = "Select Losenord from Konsulter where Anvandarnamn='" + username + "'";
+            ResultSet rs = myStmt.executeQuery(sql);
+            if (rs.next()) {
+                String tryLosen = rs.getString(1);
+                if (losen.equals(tryLosen)) {
+                    sql = "Select ID from Konsulter where Anvandarnamn='" + username + "'";
+                    rs = myStmt.executeQuery(sql);
+                    rs.next();
+                    return rs.getString(1);
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+
+    public boolean chef(String id) {
+        try {
+            Statement myStmt = conn.createStatement();
+            String sql = "Select * from Chefer where ID=" + id;
+            ResultSet rs = myStmt.executeQuery(sql);
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return false;
+        }
+    }
+
 }
